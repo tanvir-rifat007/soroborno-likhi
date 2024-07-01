96fc03eca2eee74dd454dd85bdd5dc3b8443fe1e
tanvir07-cell <tr9836859@gmail.com>
2024-06-30 21:22:06 -0700

    Chore:for the phone responsive canvas

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
-let clickX = new Array();
-let clickY = new Array();
-let clickDrag = new Array();
+let clickX = [];
+let clickY = [];
+let clickDrag = [];
 let paint;
 const labels = {
   a: "অ",
@@ -16,44 +16,22 @@ const labels = {
   k: "ঔ",
 };
 const canvas = document.getElementsByTagName("canvas")[0];
-const context = document.getElementsByTagName("canvas")[0].getContext("2d");
+const context = canvas.getContext("2d");
 const link = document.getElementById("download-link");
 
 export const resetCanvas = () => {
-  clickX = new Array();
-  clickY = new Array();
-  clickDrag = new Array();
-  paint;
+  clickX = [];
+  clickY = [];
+  clickDrag = [];
+  paint = false;
 };
 
 export const displayPrediction = (label) => {
-  let prediction = labels[label];
-
-  var predictionParagraph = document.getElementsByClassName("prediction")[0];
-
-  // if (prediction === "shorea") {
-  //   predictionParagraph.textContent = "আ";
-  // }
+  const prediction = labels[label];
+  const predictionParagraph = document.getElementsByClassName("prediction")[0];
   predictionParagraph.textContent = prediction;
   return prediction;
 };
-
-canvas.addEventListener("mousedown", function (e) {
-  paint = true;
-  addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-  redraw();
-});
-
-canvas.addEventListener("mousemove", function (e) {
-  if (paint) {
-    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
-    redraw();
-  }
-});
-
-canvas.addEventListener("mouseup", function (e) {
-  paint = false;
-});
 
 const addClick = (x, y, dragging) => {
   clickX.push(x);
@@ -70,7 +48,7 @@ const redraw = () => {
   context.fillStyle = "#ffffff";
   context.fillRect(0, 0, canvas.width, canvas.height);
 
-  for (var i = 0; i < clickX.length; i++) {
+  for (let i = 0; i < clickX.length; i++) {
     context.beginPath();
     if (clickDrag[i] && i) {
       context.moveTo(clickX[i - 1], clickY[i - 1]);
@@ -82,6 +60,59 @@ const redraw = () => {
     context.stroke();
   }
 };
+
+const getTouchPos = (canvasDom, touchEvent) => {
+  const rect = canvasDom.getBoundingClientRect();
+  return {
+    x: touchEvent.touches[0].clientX - rect.left,
+    y: touchEvent.touches[0].clientY - rect.top,
+  };
+};
+
+const handleMouseDown = (e) => {
+  paint = true;
+  addClick(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop);
+  redraw();
+};
+
+const handleMouseMove = (e) => {
+  if (paint) {
+    addClick(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop, true);
+    redraw();
+  }
+};
+
+const handleMouseUp = () => {
+  paint = false;
+};
+
+const handleTouchStart = (e) => {
+  paint = true;
+  const touchPos = getTouchPos(canvas, e);
+  addClick(touchPos.x, touchPos.y);
+  redraw();
+  e.preventDefault();
+};
+
+const handleTouchMove = (e) => {
+  if (paint) {
+    const touchPos = getTouchPos(canvas, e);
+    addClick(touchPos.x, touchPos.y, true);
+    redraw();
+  }
+  e.preventDefault();
+};
+
+const handleTouchEnd = () => {
+  paint = false;
+};
+
+canvas.addEventListener("mousedown", handleMouseDown);
+canvas.addEventListener("mousemove", handleMouseMove);
+canvas.addEventListener("mouseup", handleMouseUp);
+canvas.addEventListener("touchstart", handleTouchStart, false);
+canvas.addEventListener("touchmove", handleTouchMove, false);
+canvas.addEventListener("touchend", handleTouchEnd, false);
 
 link.addEventListener(
   "click",
